27fd5e8e55e2aa6fbc89e6167e6d2593ddd472be
Onovo Christian <onovochristian19@gmail.com>
2024-12-16 06:59:41 +0100

    Minor bug fixes nd Listings refractoring

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+// import React from 'react'
 import Cards from "./Card"
 const HomeCards = () => {
   return (
     <>
-        <section className="py-4">
+      <section className="py-4">
         <div className="container-xl lg:container m-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
             <Cards>
